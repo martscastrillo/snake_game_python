1b3545dcb44bf03dbf13af0828ee55e184e87c04
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-07 00:19:10 +0200

    add first steps

--- main.py
+++ main.py
@@ -13,13 +13,25 @@ class Food:
     pass
 def next_turn():
     pass
-def change directio();
+def change_direction(new_direction):
     pass
-def check_collisions();
+def check_collisions():
     pass
-def game_over();
+def game_over():
     pass
 
 window = Tk()
+
+window.title("Snake game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text = "Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height = GAME_HEIGHT, width = GAME_WIDTH)
+canvas.pack()
 
 window.mainloop()
