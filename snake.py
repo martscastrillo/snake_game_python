c176604b958ca6ac9f901c9d0a1c06022252051f
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-20 14:49:23 +0200

    first steps to snake ready to logic

--- snake.py
+++ snake.py
@@ -1,8 +1,19 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 screen = pygame.display.set_mode((400,500))
-
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100,200))
+test_surface.fill((0,0,255))
+test_rect = test_surface.get_rect(center = (200,250))
 while True:
-    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    # screen.fill(pygame.Color('gold'))
+    screen.fill((175,215,70))
+    pygame.draw.rect(screen, pygame.Color('red'), test_rect)
+    screen.blit(test_surface,test_rect)
     pygame.display.update()
+    clock.tick(60)
