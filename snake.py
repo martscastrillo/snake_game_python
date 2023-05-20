5403f384a8aeb5e942c1ff49e9c7fa4b1c30d8a7
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-20 15:12:02 +0200

    add first steps to snake game

--- snake.py
+++ snake.py
@@ -1,11 +1,36 @@
-import pygame, sys
+import pygame, sys,random
+from pygame.math import Vector2
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+    
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+            pygame.draw.rect(screen,(183,111,122), block_rect)
+    
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_size * cell_number,cell_size * cell_number))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100,200))
-test_surface.fill((0,0,255))
-test_rect = test_surface.get_rect(center = (200,250))
+
+fruit = FRUIT()
+snake = SNAKE()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,7 +38,7 @@ while True:
             sys.exit()
     # screen.fill(pygame.Color('gold'))
     screen.fill((175,215,70))
-    pygame.draw.rect(screen, pygame.Color('red'), test_rect)
-    screen.blit(test_surface,test_rect)
+    fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
